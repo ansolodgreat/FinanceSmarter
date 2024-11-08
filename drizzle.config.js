fe7b4f2727009076a2e0d671a2c467e8b88ae326
ansolodgreat <151402754+ansolodgreat@users.noreply.github.com>
2024-11-07 20:51:11 -0500

    Add files via upload

---
+++ drizzle.config.js
@@ -0,0 +1,10 @@
+export default {
+  dialect: "postgresql",
+  schema: "./utils/schema.jsx",
+  out: "./drizzle",
+  dbCredentials: {
+    url: "postgresql://neondb_owner:AnpF8Uy9gslq@ep-patient-poetry-a5yv0uy7.us-east-2.aws.neon.tech/neondb?sslmode=require",
+    connectionString:
+      "postgresql://neondb_owner:AnpF8Uy9gslq@ep-patient-poetry-a5yv0uy7.us-east-2.aws.neon.tech/neondb?sslmode=require",
+  },
+};
